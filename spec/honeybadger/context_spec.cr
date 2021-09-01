3ebfda725b1a1f6710bc08f5f9e27f934b148cc8
Rob <robacarp@users.noreply.github.com>
2021-09-01 15:38:46 -0600

    tests around the context system

--- spec/honeybadger/context_spec.cr
+++ spec/honeybadger/context_spec.cr
@@ -1,20 +1,46 @@
 require "../spec_helper"
-require "uuid"
 
-describe "Honeybadger.notify with context" do
-  it "allows specifying a context hash with stringable data types" do
-    exception = Honeybadger::ExamplePayload.generate_exception
+describe Honeybadger::Context do
+  before_each do
+    # also tests that a context can be cleared
+    Honeybadger::Context.current.clear
+  end
 
-    example_contexts = [
-      { "user_id" => UUID.random },
-      { "user_id" => 23 },
-      { 23 => 45 },
-      { "user_age" => 3.14 },
-      { "user_is_nil" => nil }
-    ]
+  it "allows creating a context from a hash" do
+    hash_context = {
+      "user_id" => 3
+    }
 
-    example_contexts.each do |context_hash|
-      Honeybadger.notify(exception, context: context_hash)
-    end
+    context = Honeybadger::Context.new(hash_context)
+    context["user_id"].should eq(hash_context["user_id"].to_s)
+  end
+
+  it "can pull a context from the fiber" do
+    Honeybadger::Context.current.should be(Fiber.current.honeybadger_context)
+  end
+
+  it "can store arbitrary keys and values into the context" do
+    Honeybadger.context(user_id: 45)
+    Honeybadger::Context.current["user_id"].should eq "45"
+  end
+
+  it "can merge a Log::Metadata context in" do
+    context = Honeybadger::Context.new
+    log_metadata = Log::Metadata.build({user_id: 126})
+    context["user_id"]?.should be_nil
+    context.merge log_metadata
+    context["user_id"].should eq "126"
+  end
+
+  it "can merge another Context in" do
+    context_1 = Honeybadger::Context.new
+    context_2 = Honeybadger::Context.new
+
+    context_1["user_id"] = "228"
+    context_2["shopping_cart_total"] = "48"
+
+    context_2.merge context_1
+    context_2["shopping_cart_total"].should eq "48"
+    context_2["user_id"].should eq "228"
   end
 end
