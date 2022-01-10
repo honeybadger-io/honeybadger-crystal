0985fd9758acd64706d0e85a04afc5dfb92cc83a
rob <robacarp@users.noreply.github.com>
2022-01-10 15:40:27 -0700

    updates authentichandler to work have an override-able session key, declared at init

--- src/honeybadger/authentic_handler.cr
+++ src/honeybadger/authentic_handler.cr
@@ -1,23 +1,27 @@
+require "http/server/handler"
+
 # The `Honeybadger::AuthenticHandler` is meant to integrate with the [Lucky web framework](https://luckyframework.org).
 #
 # It will attempt to fetch a user's ID from the current session, and include it in the error context if available.
-class Honeybadger::AuthenticHandler
-  include HTTP::Handler
+module Honeybadger
+  class AuthenticHandler
+    include HTTP::Handler
 
-  def initialize(@factory : Honeybadger::HttpPayload.class = Honeybadger::HttpPayload)
-  end
-
-  def call(context : HTTP::Server::Context)
-    if (user_id = context.session.get?(Authentic::ActionHelpers::SIGN_IN_KEY))
-      Honeybadger.context(user_id: user_id)
+    def initialize(*, @factory : Honeybadger::HttpPayload.class = Honeybadger::HttpPayload, @session_key : String = Authentic::ActionHelpers::SIGN_IN_KEY)
     end
 
-    call_next(context)
-  rescue exception
-    payload = @factory.new(exception, context.request)
+    def call(context : HTTP::Server::Context)
+      if user_id = context.session.get? @session_key
+        Honeybadger.context user_id: user_id
+      end
 
-    Honeybadger::Dispatch.send_async(payload)
+      call_next context
+    rescue exception
+      payload = @factory.new exception, context.request
 
-    raise exception
+      Honeybadger::Dispatch.send_async payload
+
+      raise exception
+    end
   end
 end
